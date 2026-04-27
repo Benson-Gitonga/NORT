0b1f6f240c5b63a205a2fe17ca98739e7291873c
Laureen Ndanu <laurandanu@gmail.com>
2026-04-27 17:44:24 +0300

    Changes to meta data tag

--- apps/dashboard/app/layout.js
+++ apps/dashboard/app/layout.js
@@ -1,16 +1,12 @@
-// app/layout.js or app/layout.tsx
+import Head from 'next/head';
 
-export const metadata = {
-  title: 'Nort App',
-  other: {
-    'base:app_id': '69ef725c7e92b7a4af93efb6',
-  },
-};
-
-export default function RootLayout({ children }) {
+export default function Home() {
   return (
-    <html lang="en">
-      <body>{children}</body>
-    </html>
+    <>
+      <Head>
+        <meta name="base:app_id" content="69ef725c7e92b7a4af93efb6" />
+      </Head>
+      {/* Your page content */}
+    </>
   );
 }
